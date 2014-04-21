2d7348c17bec947fc27abb2e4b081ebac4c7dabd
John Sproull <john@redmenace.org>
2014-04-21 08:17:45 -0400

    adding some unittests

--- kriek/globalsettings/tests.py
+++ kriek/globalsettings/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from kriek.globalsettings.models import GlobalSettings
+
+class GlobalSettingsTestCase(TestCase):
+	def setUp(self):
+		GlobalSettings.objects.create(key='UNITS', value='metric')
+
+	def test_defaults(self):
+		units = GlobalSettings.objects.get_setting('UNITS').value
+		self.assertEqual(units, 'metric')
