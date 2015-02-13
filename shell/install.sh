6e1d912b490d1fa6d3feacd973401ff894e99d0b
John Sproull <john@redmenace.org>
2015-02-13 10:50:15 -0500

    updated install script so it works with Raspberry Pi 2. Will update it to auto detect if Device Trees are installed.

--- shell/install.sh
+++ shell/install.sh
@@ -10,9 +10,9 @@ sudo apt-get upgrade -y
 sudo apt-get install libpq-dev python-dev postgresql-server-dev-9.1  postgresql postgresql-contrib nginx supervisor python-virtualenv -y
 
 # for BBB
-sudo apt-get install build-essential python-setuptools python-pip python-smbus -y
+#sudo apt-get install build-essential python-setuptools python-pip python-smbus -y
 # for bbb
-sudo adduser pi
+#sudo adduser pi
 
 sudo mkdir /opt/kriek
 sudo chown pi /opt/kriek
@@ -33,22 +33,27 @@ git clone https://github.com/jsproull/kriek.git
 # /opt/kriek/env-kriek/bin/pip install Adafruit_BBIO
 
 #conf postgres
-sudo su postgres -c ~pi/psql.sh
+sudo su postgres -c /opt/kriek/kriek/shell/psql.sh
 
 #conf django
 cd /opt/kriek/kriek
 ./manage.py syncdb
-./manage.py migrate common
-./manage.py migrate brew
-./manage.py migrate ferm
-./manage.py migrate globalsettings
-./manage.py migrate status
+#./manage.py migrate common
+#./manage.py migrate brew
+#./manage.py migrate ferm
+#./manage.py migrate globalsettings
+#./manage.py migrate status
 
 sudo ./manage.py collectstatic --noinput
 
 #then set up gunicorn, supervisord and nginx
+sudo rm /etc/nginx/sites-enabled/default
 sudo cp -R /opt/kriek/kriek/conf/ngnix/* /etc/nginx/
 sudo cp -R /opt/kriek/kriek/conf/supervisor/conf.d/* /etc/supervisor/conf.d/
 
-# and do
-sudo sh -c "echo 'w1_gpio\nw1_therm\n' >> /etc/modules"
+# raspberry pi modules
+# sudo sh -c "echo 'w1_gpio\nw1_therm\n' >> /etc/modules"
+
+#raspberry pi device tree
+sudo sh -c "echo '#one wire\ndtoverlay=w1-gpio\n' >> /boot/config.txt"
+
